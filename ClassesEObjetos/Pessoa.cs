683d6d1a7929fac4932dbb975f7ff65526065b38
Moisés Mariano <moisesduartem@gmail.com>
2022-04-05 13:07:56 -0300

    creating an instance & setting properties

---
+++ ClassesEObjetos/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassesEObjetos
+{
+    class Pessoa
+    {
+        public string nome;
+        public int idade;
+        public string genero;
+        
+        public void Identificar()
+        {
+            System.Console.WriteLine($"Olá, sou o {nome}, tenho {idade} e sou do gênero {genero}");
+        }
+    }
+}
---
+++ ClassesEObjetos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassesEObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            pessoa1.nome = "Paulo";
+            pessoa1.idade = 45;
+            pessoa1.genero = "Masculino";
+
+            pessoa1.Identificar();
+
+            //Pessoa pessoa2 = new Pessoa();
+            //pessoa2.nome = "José";
+            //Console.WriteLine(pessoa2.nome);
+        }
+    }
+}
